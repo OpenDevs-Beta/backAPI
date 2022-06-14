48b41194e08304e9c52e20bf7b5bb67b7ed78445
Victor Canas <victorcanas972@gmail.com>
2022-06-14 15:11:32 +0200

    Fixed empresas migration

--- database/migrations/1655212016387_empresas.ts
+++ database/migrations/1655212016387_empresas.ts
@@ -1,19 +1,24 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+import { TipoSector } from 'App/Models/Contracts/TipoSector'
 
 export default class extends BaseSchema {
   protected tableName = 'empresas'
 
   public async up() {
-    this.schema.createTable(this.tableName, (table) => {
-      table.increments('id')
+    this.schema.alterTable(this.tableName, (table) => {
+      table.increments('id').primary()
+
       table.string('nombre').notNullable().unique()
       table.text('descripcion').notNullable()
       table.text('enlace_web')
       table.text('enlace_twitter')
       table.text('enlace_linkedin')
       table.string('num_empleados')
-      table.integer('creacion')
+      table.string('avatar')
+      table.enu('sector', Object.values(TipoSector)).defaultTo(TipoSector.INFORMATICA).notNullable()
+      table.date('creacion').notNullable()
 
+      table.integer('user_id').unsigned().references('users.id').index()
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
