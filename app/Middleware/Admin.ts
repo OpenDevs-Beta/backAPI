8f12694fadc00c0922e93b377e763d8736b6c298
Victor Canas <victorcanas972@gmail.com>
2022-06-16 10:47:26 +0200

    Added admin middleware with its permission except

---
+++ app/Middleware/Admin.ts
@@ -0,0 +1,13 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import PermissionException from 'App/Exceptions/PermissionException'
+import { TipoRol } from 'App/Models/Contracts/TipoRol'
+
+export default class AdminMiddleware {
+  public async handle({ auth }: HttpContextContract, next: () => Promise<void>) {
+    if (auth.user?.$attributes.role !== TipoRol.ADMIN) {
+      throw new PermissionException('You are not authorized', 403, 'E_UNAUTHORIZED')
+    }
+
+    await next()
+  }
+}
