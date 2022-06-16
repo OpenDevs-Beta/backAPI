54834fa099089019b7157ef4dacd561958407d4c
Victor Canas <victorcanas972@gmail.com>
2022-06-16 19:43:25 +0200

    Fixed admin middleware

--- app/Middleware/Admin.ts
+++ app/Middleware/Admin.ts
@@ -4,7 +4,7 @@ import { TipoRol } from 'App/Models/Contracts/TipoRol'
 
 export default class AdminMiddleware {
   public async handle({ auth }: HttpContextContract, next: () => Promise<void>) {
-    if (auth.user?.$attributes.role !== TipoRol.ADMIN) {
+    if (auth.user?.rol !== TipoRol.ADMIN) {
       throw new PermissionException('You are not authorized', 403, 'E_UNAUTHORIZED')
     }
 
